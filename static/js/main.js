b82614abad0f8c341d462245949295cedcee084d
tinrab <tin.rabzelj@gmail.com>
2017-07-18 05:59:51 +0200

    connect newsletter

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+  var $newsletterSuccess = $('#newsletterSuccess');
+  var $newsletterError = $('#newsletterError');
+
+  $('#subscribeButton').click(function () {
+    var email = $('#emailInput').val();
+    var pt = 'tQswM96k127VkJZ88mor2HFLYaDFOgjp';
+    var url = 'https://outcrawl-newsletter.appspot.com/subscribe?email=' + email + '&token=' + pt;
+
+    $.post(url)
+      .done(function (data) {
+        $newsletterError.hide();
+        $newsletterSuccess.show();
+      })
+      .fail(function (xhr, status, error) {
+        $newsletterSuccess.hide();
+        $newsletterError.show();
+      });
+  });
+
   $('.katex').each(function (i, obj) {
     obj.innerHTML = katex.renderToString(obj.innerText);
   });
