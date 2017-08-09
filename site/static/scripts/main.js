7f9474dc541af8a3fe9190222c3763a0ac314a04
tinrab <tin.rabzelj@gmail.com>
2017-08-09 10:29:33 +0200

    change header

--- site/static/scripts/main.js
+++ site/static/scripts/main.js
@@ -3,9 +3,9 @@ const Clipboard = require('clipboard');
 
 mdc.autoInit();
 
-let buttons = document.querySelector('.button');
-if (buttons) {
-  mdc.ripple.MDCRipple.attachTo(buttons);
+let buttons = document.querySelectorAll('.button, .tag');
+for (let b of buttons) {
+  mdc.ripple.MDCRipple.attachTo(b);
 }
 
 new Clipboard('.icon-button[data-clipboard-text]');
