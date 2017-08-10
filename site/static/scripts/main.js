72899314f4cafbecb3751d5162dc4e73b4a040f6
tinrab <tin.rabzelj@gmail.com>
2017-08-10 11:40:22 +0200

    style tables

--- site/static/scripts/main.js
+++ site/static/scripts/main.js
@@ -3,9 +3,4 @@ const Clipboard = require('clipboard');
 
 mdc.autoInit();
 
-let buttons = document.querySelectorAll('.button, .tag');
-for (let b of buttons) {
-  mdc.ripple.MDCRipple.attachTo(b);
-}
-
 new Clipboard('.icon-button[data-clipboard-text]');
