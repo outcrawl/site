8dc5086e24813f1d697f54019cf27e98c16d3af7
Tin Rabzelj <tin@flinect.com>
2023-01-02 02:07:19 +0100

    Add warning

--- src/common/Page.tsx
+++ src/common/Page.tsx
@@ -1,8 +1,9 @@
+import { Alert, Container } from '@mui/material';
+import React, { ReactNode } from 'react';
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
 import CookieConsent from './CookieConsent';
-import { Container } from '@mui/material';
-import React, { ReactNode } from 'react';
+import ExternalLink from './ExternalLink';
 
 type PageProps = {
   narrow?: boolean;
@@ -12,6 +13,9 @@ type PageProps = {
 const Page: React.FC<PageProps> = ({ narrow, children }: PageProps) => (
   <>
     <Header />
+    <Alert severity="warning" sx={{ m: 2 }}>
+      This website is deprecated. Go to <ExternalLink href="https://flinect.com">flinect.com</ExternalLink>!
+    </Alert>
     <Container
       sx={{ flexGrow: 1, my: 3 }}
       component="main"
